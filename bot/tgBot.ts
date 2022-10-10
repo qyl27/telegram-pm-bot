bc9ad78e83e2dafb49ca06cf705a3e8910a8da98
秋雨落 <i@rain.cx>
2022-10-10 21:42:14 +0800

    :sparkles: It works.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- bot/tgBot.ts
+++ bot/tgBot.ts
@@ -1,67 +1,161 @@
-import TelegramBot, {Message, SendMessageOptions} from "node-telegram-bot-api";
-import {TG_TOKEN} from "./config";
+import {ADMIN_ID, TG_TOKEN} from "./config";
+import {Context, Telegraf} from "telegraf";
+import { Update, MessageEntity, Message } from "typegram";
+import {User} from "typegram/manage";
 
 class TgBot {
-    bot: TelegramBot;
+    bot: Telegraf;
 
     constructor(token: string) {
-        this.bot = new TelegramBot(token, {polling: false, webHook: false});
+        this.bot = new Telegraf(token);
 
-        this.bot.onText(/\/echo/, (msg, match) => this.onEchoEmpty(msg, match));
-        this.bot.onText(/\/echo (.+)/, (msg, match) => this.onEcho(msg, match));
+        this.bot.hears(/\/echo (.+)/, ctx => this.onEcho(ctx));
 
-        this.bot.onText(/\/start/, (msg, match) => this.onStart(msg, match));
-        this.bot.on('text', msg => this.onReply(msg))
+        this.bot.start(ctx => this.onStart(ctx));
+        this.bot.on('text', ctx => this.onText(ctx))
     }
 
-    public processUpdate(body: any) {
-        this.bot.processUpdate(body);
+    public async processUpdate(body: any) {
+        await this.bot.handleUpdate(body);
     }
 
-    public onEchoEmpty(message: Message, matches: RegExpExecArray) {
-        this.replyTo(message.chat.id, message.message_id, "我应该说些什么呢？");
+    protected onEcho(context) {
+        if (context.match.length > 1) {
+            this.replyTo(context, context.match[1]);
+        }
     }
 
-    public onEcho(message: Message, matches: RegExpExecArray) {
-        this.replyTo(message.chat.id, message.message_id, matches[1]);
+    protected onStart(context: Context<Update.MessageUpdate>) {
+        let msg = context.message;
+
+        if (msg.chat.type === 'private' && msg.from.id === ADMIN_ID) {
+            this.replyTo(context, "我在~");
+        } else {
+            this.replyTo(context, "欢迎联系私聊 Bot ，我已经打开了一条转发通道至我的 Owner 。\n回复我的消息就可以进行私聊了。");
+        }
     }
 
-    public onStart(message: Message, matches: RegExpExecArray) {
-        this.replyTo(message.chat.id, message.message_id, "欢迎联系私聊 Bot ，我已经打开了一条转发通道至我的 Owner 。\n回复这条消息就可以开始私聊了。");
+    protected onText(context: Context<Update.MessageUpdate>) {
+        if ('reply_to_message' in context.message) {
+            this.onReply(context);
+        }
     }
 
-    public onReply(message: Message) {
-        if (message.reply_to_message) {
-            let cursor = message.reply_to_message;
-            let depth = 1;
-            while (cursor.reply_to_message) {
-                cursor = cursor.reply_to_message;
-                depth += 1;
+    protected onReply(context: Context<Update.MessageUpdate>) {
+        let msg = context.message;
+
+        if ('reply_to_message' in msg) {
+            let reply = msg.reply_to_message;
+
+            if (msg.chat.type === 'private' && msg.from.id !== ADMIN_ID) {
+                if ('text' in msg) {
+                    this.forwardMessage(msg.from, ADMIN_ID, msg.text);
+                } else {
+                    // Todo: qyl27: More message type.
+                    this.replyTo(context, "暂时不支持其他类型的消息呢，请使用文本消息。");
+                }
             }
 
-            let root = cursor;
-            if (root.text === '/start') {
-                // Todo
+            if (msg.chat.type === 'private' && msg.from.id === ADMIN_ID) {
+                let targetId: string;
+
+                if ('text' in reply) {
+                    let arr1 = reply.text.split("【");
+                    let arr2 = arr1[1].split("】");
+                    targetId = arr2[0];
+                }
+
+                if (targetId) {
+                    this.forwardMessageReverse(Number(targetId).valueOf(), msg.chat.id, msg.message_id);
+                } else {
+                    this.replyTo(context, "错误：找不到对应的用户。");
+                }
             }
         }
     }
 
-    public sendMessage(chatId: number, text: string) {
-        this.sendMessageInternal(chatId, text, 0);
+    public forwardMessage(user: User, chatId: number, text: string) {
+        let username = user.first_name + (user.last_name ? ' ' + user.last_name : '');
+        let userId = user.username ? ` (@${user.username}) :` : '';
+        let prefixLine = `${username} ${userId}`;
+
+        this.forwardMessageInternal(chatId, `【${user.id}】${prefixLine}\n${text}`, [], 0);
     }
 
-    public replyTo(chatId: number, replyId: number, text: string) {
-        this.sendMessageInternal(chatId, text, 0, { reply_to_message_id: replyId });
-    }
-
-    private sendMessageInternal(chatId: number, text: string, attempt: number, options?: SendMessageOptions) {
+    private forwardMessageInternal(chatId: number, text: string, entities: MessageEntity[], attempt: number) {
         if (attempt < 5) {
-            this.bot.sendMessage(chatId, text, options)
+            this.bot.telegram.sendMessage(chatId, text, {
+                entities: entities,
+                // parse_mode: 'HTML'
+            })
                 .catch(err => {
                     console.log(err);
-                    console.log(`Attempt: ${attempt}.`);
-                    this.sendMessageInternal(chatId, text, attempt + 1, options);
-                })
+                    console.log(`Forwarding message to ${chatId}. Attempt: ${attempt}.`);
+                    this.forwardMessageInternal(chatId, text, entities, attempt + 1);
+                });
+                // .then(res => {
+                //     console.log(res)
+                // })
+        }
+    }
+
+    public forwardMessageReverse(chatId: number, fromChatId: number, msgId: number) {
+        this.forwardMessageReverseInternal(chatId, fromChatId, msgId, 0);
+    }
+
+    private forwardMessageReverseInternal(chatId: number, fromChatId: number, msgId: number, attempt: number) {
+        if (attempt < 5) {
+            this.bot.telegram.forwardMessage(chatId, fromChatId, msgId)
+                .catch(err => {
+                    console.log(err);
+                    console.log(`Forwarding message to ${chatId}. Attempt: ${attempt}.`);
+                    this.forwardMessageReverseInternal(chatId, fromChatId, msgId, attempt + 1);
+                });
+        }
+    }
+
+    public sendMessageTo(chatId: number, text: string) {
+        this.sendMessageToInternal(chatId, text, 0);
+    }
+
+    public sendMessage(context: Context, text: string) {
+        this.sendMessageInternal(context, text, 0);
+    }
+
+    public replyTo(context: Context<Update.MessageUpdate>, text: string) {
+        this.replyToInternal(context, text, 0);
+    }
+
+    private sendMessageToInternal(chatId: number, text: string, attempt: number) {
+        if (attempt < 5) {
+            this.bot.telegram.sendMessage(chatId, text)
+                .catch(err => {
+                    console.log(err);
+                    console.log(`Sending message to ${chatId}. Attempt: ${attempt}.`);
+                    this.sendMessageToInternal(chatId, text, attempt + 1);
+                });
+        }
+    }
+
+    private sendMessageInternal(context: Context, text: string, attempt: number) {
+        if (attempt < 5) {
+            context.sendMessage(text)
+                .catch(err => {
+                    console.log(err);
+                    console.log(`Sending message to context: ${context.chat.id}. Attempt: ${attempt}.`);
+                    this.sendMessageInternal(context, text, attempt + 1);
+                });
+        }
+    }
+
+    private replyToInternal(context: Context<Update.MessageUpdate>, text: string, attempt: number) {
+        if (attempt < 5) {
+            context.telegram.sendMessage(context.chat.id, text, {reply_to_message_id: context.message.message_id})
+                .catch(err => {
+                    console.log(err);
+                    console.log(`Replying message to context: ${context.chat.id}. Attempt: ${attempt}.`);
+                    this.replyToInternal(context, text, attempt + 1);
+                });
         }
     }
 }
--- pages/api/update.ts
+++ pages/api/update.ts
@@ -2,11 +2,11 @@ import {NextApiRequest, NextApiResponse} from "next";
 import {SECRET_TOKEN} from "../../bot/config";
 import tgBot from "../../bot/tgBot";
 
-export default function onUpdate(req: NextApiRequest, res: NextApiResponse) {
+export default async function onUpdate(req: NextApiRequest, res: NextApiResponse) {
     let header = req.headers['x-telegram-bot-api-secret-token'];
 
     if (header === SECRET_TOKEN) {
-        tgBot.processUpdate(req.body);
+        await tgBot.processUpdate(req.body);
 
         res.status(200).json("True");
         return;
